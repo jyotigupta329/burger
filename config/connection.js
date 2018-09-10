2644753502eaa1adf569fd60738594fc54091ea1
jyoti <jyoti.gupta329@gmail.com>
2018-09-09 19:49:18 -0500

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) {
         console.log(" error connecting: " + err.stack);
